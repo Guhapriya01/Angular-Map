082ee6e6de8d7b2c00a11dae1ee8a16468bb79ba
Guhapriya01 <www.guhadharma@gmail.com>
2022-12-01 08:04:31 +0530

    commit

---
+++ src/app/places/list-places/list-places.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-places',
+  templateUrl: './list-places.component.html',
+  styleUrls: ['./list-places.component.css']
+})
+export class ListPlacesComponent {
+
+}
